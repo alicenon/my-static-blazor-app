be26942a5e18e92197592a3773896772b9921af7
Alejandro <77890322+alicenon@users.noreply.github.com>
2024-04-06 00:09:58 +0200

    Update Program.cs
    test pull

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
-
+//listo para el pull
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
